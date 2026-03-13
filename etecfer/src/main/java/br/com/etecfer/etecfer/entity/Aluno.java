c0284ea621ecafd189df7dabec890b44fbcdcc24
vee-lgtv <veronicanunesnues70@gmail.com>
2026-03-13 14:08:04 -0300

    Implementação do arquivo application.propperties e execução do projeto

--- etecfer/src/main/java/br/com/etecfer/etecfer/entity/Aluno.java
+++ etecfer/src/main/java/br/com/etecfer/etecfer/entity/Aluno.java
@@ -5,8 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Aluno {
 //Definição dos atributoa da entidade Aluno
     @Id
@@ -27,73 +35,5 @@ public class Aluno {
 
     @Column(nullable = false, length = 11)
     private String cpfAluno;
-
-     //Métodos construtores
-
-    public Aluno() {
-    }
-
-    public Aluno(Integer idAluno, String nomeAluno, String emailAluno, String telefoneAluno, Integer raAluno,
-            String cpfAluno) {
-        this.idAluno = idAluno;
-        this.nomeAluno = nomeAluno;
-        this.emailAluno = emailAluno;
-        this.telefoneAluno = telefoneAluno;
-        this.raAluno = raAluno;
-        this.cpfAluno = cpfAluno;
-    }
-  
-    // Métodos acessores (getters e setters) 
-
-    public Integer getIdAluno() {
-        return idAluno;
-    }
-
-    public void setIdAluno(Integer idAluno) {
-        this.idAluno = idAluno;
-    }
-
-    public String getNomeAluno() {
-        return nomeAluno;
-    }
-
-    public void setNomeAluno(String nomeAluno) {
-        this.nomeAluno = nomeAluno;
-    }
-
-    public String getEmailAluno() {
-        return emailAluno;
-    }
-
-    public void setEmailAluno(String emailAluno) {
-        this.emailAluno = emailAluno;
-    }
-
-    public String getTelefoneAluno() {
-        return telefoneAluno;
-    }
-
-    public void setTelefoneAluno(String telefoneAluno) {
-        this.telefoneAluno = telefoneAluno;
-    }
-
-    public Integer getRaAluno() {
-        return raAluno;
-    }
-
-    public void setRaAluno(Integer raAluno) {
-        this.raAluno = raAluno;
-    }
-
-    public String getCpfAluno() {
-        return cpfAluno;
-    }
-
-    public void setCpfAluno(String cpfAluno) {
-        this.cpfAluno = cpfAluno;
-    }
-
-   
-    
-
+     
 }
